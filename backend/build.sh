49895788a75dcac4daac0cedbd75c993251b47ec
Atharv3808 <142673706+Atharv3808@users.noreply.github.com>
2026-03-05 11:11:57 +0530

    Deploy backend

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Run migrations and collectstatic
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --no-input
